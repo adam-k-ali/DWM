08c0a2573e37e76ba1b2cb454eb84f14777cc1f2
Adam Ali <adamka2000@outlook.com>
2024-11-26 23:46:03 +0000

    Remove example mod stubs

---
+++ src/main/java/com/adamkali/opendwm/DWMMain.java
@@ -0,0 +1,42 @@
+package com.adamkali.opendwm;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+@Mod(DWMMain.MODID)
+public class DWMMain {
+    public static final String MODID = "open_dwm";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public DWMMain(FMLJavaModLoadingContext context) {
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+        IEventBus modEventBus = context.getModEventBus();
+        registerDeferredRegistries(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void registerDeferredRegistries(IEventBus modEventBus) {
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+    }
+}
