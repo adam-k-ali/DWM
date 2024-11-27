65e56e19e913f29916fb3783cc205a7948f5f186
Adam Ali <adamka2000@outlook.com>
2024-11-27 21:44:50 +0000

    Play sonic screwdriver sound on performing action

--- src/main/java/com/adamkali/opendwm/actions/SonicActions.java
+++ src/main/java/com/adamkali/opendwm/actions/SonicActions.java
@@ -1,6 +1,9 @@
 package com.adamkali.opendwm.actions;
 
+import com.adamkali.opendwm.sound.DWMSounds;
 import net.minecraft.world.item.context.UseOnContext;
+
+import java.util.Objects;
 
 public class SonicActions {
     private static SonicActions INSTANCE;
@@ -17,6 +20,7 @@ public class SonicActions {
     }
 
     public void tryPerformAction(UseOnContext context) {
+        context.getLevel().playLocalSound(context.getClickedPos(), DWMSounds.SONIC_THIRD_DOCTOR.get(), Objects.requireNonNull(context.getPlayer()).getSoundSource(), 1.0F, 1.0F, false);
         System.out.println("SonicActions.tryPerformAction");
     }
 
