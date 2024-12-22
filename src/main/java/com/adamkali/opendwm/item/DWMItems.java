82fec232387219ef6425ca3ad855932ba9c4eb6c
Adam Ali <adamka2000@outlook.com>
2024-12-22 21:37:51 +0000

    Add fourth doctor's sonic screwdriver

--- src/main/java/com/adamkali/opendwm/item/DWMItems.java
+++ src/main/java/com/adamkali/opendwm/item/DWMItems.java
@@ -13,11 +13,14 @@ import net.minecraftforge.registries.RegistryObject;
 public class DWMItems {
     public static final RegistryObject<Item> SONIC_THIRD_DOCTOR = DWMMain.ITEMS.register("sonic_third_doctor",
             () -> new SonicScrewdriverItem(new Item.Properties().setId(DWMMain.ITEMS.key("sonic_third_doctor"))));
+    public static final RegistryObject<Item> SONIC_FOURTH_DOCTOR = DWMMain.ITEMS.register("sonic_fourth_doctor",
+            () -> new SonicScrewdriverItem(new Item.Properties().setId(DWMMain.ITEMS.key("sonic_fourth_doctor"))));
 
     @SubscribeEvent
     public static void buildCreativeTabContents(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(SONIC_THIRD_DOCTOR);
+            event.accept(SONIC_FOURTH_DOCTOR);
         }
     }
 
