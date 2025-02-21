e14b3e63ff7dcf8e4978a6e1d2db53526ee0db59
Adam Ali <adamka2000@outlook.com>
2025-02-21 22:06:08 +0000

    Add sonic screwdrivers to tools creative tab

--- src/main/java/com/adamkali/dwm/item/DWMItems.java
+++ src/main/java/com/adamkali/dwm/item/DWMItems.java
@@ -1,7 +1,9 @@
 package com.adamkali.dwm.item;
 
 import com.adamkali.dwm.DWMReference;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -28,6 +30,12 @@ public class DWMItems {
 
 
     public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
+            content.add(SONIC_SECOND_DOCTOR);
+            content.add(SONIC_THIRD_DOCTOR);
+            content.add(SONIC_FOURTH_DOCTOR);
+            content.add(SONIC_FIFTH_DOCTOR);
+        });
     }
 
     public static Item register(Function<Item.Settings, Item> item, String id) {
