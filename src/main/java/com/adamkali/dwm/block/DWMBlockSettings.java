1e3b3b01b2c64386e6e5335146027ed4972ec3e6
Adam Ali <adamka2000@outlook.com>
2025-03-11 08:20:11 +0000

    DWM-61: Add Big Roundel

---
+++ src/main/java/com/adamkali/dwm/block/DWMBlockSettings.java
@@ -0,0 +1,8 @@
+package com.adamkali.dwm.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.sound.BlockSoundGroup;
+
+public class DWMBlockSettings {
+    public static final AbstractBlock.Settings ROUNDEL_BLOCK_SETTINGS = AbstractBlock.Settings.create().strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD);
+}
