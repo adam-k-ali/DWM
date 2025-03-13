57494bf3a902dcee8e24dadeed2e1a707bc693e3
Adam Ali <adamka2000@outlook.com>
2025-03-13 22:49:52 +0000

    DWM-32: Add white and teal TARDIS wall block

--- src/main/java/com/adamkali/dwm/block/DWMBlocks.java
+++ src/main/java/com/adamkali/dwm/block/DWMBlocks.java
@@ -21,6 +21,8 @@ public class DWMBlocks {
     public static final Block WHITE_BIG_ROUNDEL_B = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS.nonOpaque(), "white_big_roundel_b");
     public static final Block WHITE_EXTRUDED_ROUNDEL_A = register(DWMHorizontalFacingBlock::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "white_extruded_roundel_a");
     public static final Block WHITE_EXTRUDED_ROUNDEL_B = register(DWMHorizontalFacingBlock::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS.nonOpaque(), "white_extruded_roundel_b");
+    public static final Block WHITE_TARDIS_WALL = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "white_tardis_wall");
+    public static final Block TEAL_TARDIS_WALL = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "teal_tardis_wall");
 
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
@@ -30,6 +32,8 @@ public class DWMBlocks {
             content.add(WHITE_BIG_ROUNDEL_B.asItem());
             content.add(WHITE_EXTRUDED_ROUNDEL_A.asItem());
             content.add(WHITE_EXTRUDED_ROUNDEL_B.asItem());
+            content.add(WHITE_TARDIS_WALL.asItem());
+            content.add(TEAL_TARDIS_WALL.asItem());
         });
     }
 
