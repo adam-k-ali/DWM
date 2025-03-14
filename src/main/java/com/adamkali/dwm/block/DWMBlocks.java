1cae8c7b51ec7bd4b19c57f13000f9123391b1db
Adam Ali <adamka2000@outlook.com>
2025-03-14 08:32:49 +0000

    DWM-32: Add Dark Tardis Wall & Roundels

--- src/main/java/com/adamkali/dwm/block/DWMBlocks.java
+++ src/main/java/com/adamkali/dwm/block/DWMBlocks.java
@@ -15,18 +15,23 @@ import net.minecraft.util.Identifier;
 import java.util.function.Function;
 
 public class DWMBlocks {
-    public static final Block WHITE_ROUNDEL_A = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "white_roundel_a");
-    public static final Block WHITE_ROUNDEL_B = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS.nonOpaque(), "white_roundel_b");
-    public static final Block TEAL_ROUNDEL_A = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "teal_roundel_a");
-    public static final Block TEAL_ROUNDEL_B = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS.nonOpaque(), "teal_roundel_b");
-    public static final Block WHITE_BIG_ROUNDEL_A = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "white_big_roundel_a");
-    public static final Block WHITE_BIG_ROUNDEL_B = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS.nonOpaque(), "white_big_roundel_b");
-    public static final Block TEAL_BIG_ROUNDEL_A = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "teal_big_roundel_a");
-    public static final Block TEAL_BIG_ROUNDEL_B = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS.nonOpaque(), "teal_big_roundel_b");
-    public static final Block WHITE_EXTRUDED_ROUNDEL_A = register(DWMHorizontalFacingBlock::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "white_extruded_roundel_a");
-    public static final Block WHITE_EXTRUDED_ROUNDEL_B = register(DWMHorizontalFacingBlock::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS.nonOpaque(), "white_extruded_roundel_b");
-    public static final Block WHITE_TARDIS_WALL = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "white_tardis_wall");
-    public static final Block TEAL_TARDIS_WALL = register(Block::new, DWMBlockSettings.ROUNDEL_BLOCK_SETTINGS, "teal_tardis_wall");
+    public static final Block WHITE_ROUNDEL_A = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "white_roundel_a");
+    public static final Block WHITE_ROUNDEL_B = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS.nonOpaque(), "white_roundel_b");
+    public static final Block TEAL_ROUNDEL_A = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "teal_roundel_a");
+    public static final Block TEAL_ROUNDEL_B = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS.nonOpaque(), "teal_roundel_b");
+    public static final Block DARK_ROUNDEL_A = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "dark_roundel_a");
+    public static final Block DARK_ROUNDEL_B = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS.nonOpaque(), "dark_roundel_b");
+    public static final Block WHITE_BIG_ROUNDEL_A = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "white_big_roundel_a");
+    public static final Block WHITE_BIG_ROUNDEL_B = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS.nonOpaque(), "white_big_roundel_b");
+    public static final Block TEAL_BIG_ROUNDEL_A = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "teal_big_roundel_a");
+    public static final Block TEAL_BIG_ROUNDEL_B = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS.nonOpaque(), "teal_big_roundel_b");
+    public static final Block DARK_BIG_ROUNDEL_A = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "dark_big_roundel_a");
+    public static final Block DARK_BIG_ROUNDEL_B = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS.nonOpaque(), "dark_big_roundel_b");
+    public static final Block WHITE_EXTRUDED_ROUNDEL_A = register(DWMHorizontalFacingBlock::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "white_extruded_roundel_a");
+    public static final Block WHITE_EXTRUDED_ROUNDEL_B = register(DWMHorizontalFacingBlock::new, DWMBlockSettings.TARDIS_WALL_SETTINGS.nonOpaque(), "white_extruded_roundel_b");
+    public static final Block WHITE_TARDIS_WALL = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "white_tardis_wall");
+    public static final Block TEAL_TARDIS_WALL = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "teal_tardis_wall");
+    public static final Block DARK_TARDIS_WALL = register(Block::new, DWMBlockSettings.TARDIS_WALL_SETTINGS, "dark_tardis_wall");
 
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
@@ -34,14 +39,19 @@ public class DWMBlocks {
             content.add(WHITE_ROUNDEL_B.asItem());
             content.add(TEAL_ROUNDEL_A.asItem());
             content.add(TEAL_ROUNDEL_B.asItem());
+            content.add(DARK_ROUNDEL_A.asItem());
+            content.add(DARK_ROUNDEL_B.asItem());
             content.add(WHITE_BIG_ROUNDEL_A.asItem());
             content.add(WHITE_BIG_ROUNDEL_B.asItem());
             content.add(TEAL_BIG_ROUNDEL_A.asItem());
             content.add(TEAL_BIG_ROUNDEL_B.asItem());
+            content.add(DARK_BIG_ROUNDEL_A.asItem());
+            content.add(DARK_BIG_ROUNDEL_B.asItem());
             content.add(WHITE_EXTRUDED_ROUNDEL_A.asItem());
             content.add(WHITE_EXTRUDED_ROUNDEL_B.asItem());
             content.add(WHITE_TARDIS_WALL.asItem());
             content.add(TEAL_TARDIS_WALL.asItem());
+            content.add(DARK_TARDIS_WALL.asItem());
         });
     }
 
