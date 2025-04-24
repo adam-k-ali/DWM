b5ecbe7784affcb6c07bb31a57cea2c1d599588f
Adam Ali <adamka2000@outlook.com>
2025-04-24 20:31:51 +0100

    Rotate TARDIS Block based on how it's placed

--- src/client/java/com/adamkali/dwm/model/tileentity/TardisBlockEntityRenderer.java
+++ src/client/java/com/adamkali/dwm/model/tileentity/TardisBlockEntityRenderer.java
@@ -1,6 +1,8 @@
 package com.adamkali.dwm.model.tileentity;
 
+import com.adamkali.dwm.block.TardisBlock;
 import com.adamkali.dwm.block.entities.TardisBlockEntity;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -8,6 +10,7 @@ import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.RotationAxis;
+import net.minecraft.util.math.RotationPropertyHelper;
 
 public class TardisBlockEntityRenderer implements BlockEntityRenderer<TardisBlockEntity> {
     private final TTCapsuleModel ttCapsuleModel;
@@ -17,17 +20,21 @@ public class TardisBlockEntityRenderer implements BlockEntityRenderer<TardisBloc
 
     @Override
     public void render(TardisBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        BlockState state = entity.getCachedState();
+        int rotation = state.get(TardisBlock.FACING_ROTATION, 0);
+
         VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntitySolid(TTCapsuleModel.TEXTURE_LOCATION));
-        this.render(matrices, vertexConsumer, this.ttCapsuleModel, entity.getDoorState().getDoorSwing(), light, overlay);
+        this.render(matrices, vertexConsumer, this.ttCapsuleModel, entity.getDoorState().getDoorSwing(), RotationPropertyHelper.toDegrees(rotation), light, overlay);
     }
 
-    private void render(MatrixStack matrices, VertexConsumer vertices, TTCapsuleModel model, float doorProgress, int light, int overlay) {
+    private void render(MatrixStack matrices, VertexConsumer vertices, TTCapsuleModel model, float doorProgress, float rotation, int light, int overlay) {
         TTCapsuleRenderState state = new TTCapsuleRenderState();
         state.setDoorSwingProgress(doorProgress);
         matrices.push();
         matrices.scale(2.0f, 2.0f, 2.0f);
         matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180.0f));
         matrices.translate(0.25D, -1.5D, -0.25D);
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(rotation - 180.0f));
         model.setAngles(state);
         model.render(matrices, vertices, light, overlay);
         matrices.pop();
--- src/main/java/com/adamkali/dwm/block/TardisBlock.java
+++ src/main/java/com/adamkali/dwm/block/TardisBlock.java
@@ -3,28 +3,54 @@ package com.adamkali.dwm.block;
 import com.adamkali.dwm.block.entities.DWMBlockEntities;
 import com.adamkali.dwm.block.entities.TardisBlockEntity;
 import com.mojang.serialization.MapCodec;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RotationPropertyHelper;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class TardisBlock extends BlockWithEntity {
+    public static final IntProperty FACING_ROTATION = Properties.ROTATION;
+
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(-10.0, 0.0, -10.0, 26.0, 48.0, 26.0);
+
     public TardisBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING_ROTATION, 0));
     }
 
     @Override
     protected MapCodec<? extends BlockWithEntity> getCodec() {
         return createCodec(TardisBlock::new);
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public @Nullable BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING_ROTATION, RotationPropertyHelper.fromYaw(ctx.getPlayerYaw()));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(FACING_ROTATION);
     }
 
     @Override
@@ -51,7 +77,6 @@ public class TardisBlock extends BlockWithEntity {
 
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-        // Make sure to check world.isClient if you only want to tick only on serverside.
         return validateTicker(type, DWMBlockEntities.TARDIS_BLOCK_ENTITY, (world1, pos, state1, blockEntity) -> {
             blockEntity.tick(world1, pos, state1, blockEntity);
         });
